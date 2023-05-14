170dcf96654697754eac64256020761718570453
dawid <dawid.steciuk@gmail.com>
2023-05-14 12:56:27 +0200

    nie wiem, zabijcie mnie

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false, 
-      home: ListPage(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ExpandableListApp(),
     );
   }
-  }
+}
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,22 +1,88 @@
 import 'package:flutter/material.dart';
 
-class ListPage extends StatelessWidget {
-  const ListPage({super.key});
+class Room {
+  final int building;
+  final int floor;
+  final int roomNumber;
 
-  static var dataHere=<String>['PlaceHolder1', 'PlaceHolder2'];
+  Room(this.building, this.floor, this.roomNumber);
+}
+
+class ExpandableListApp extends StatelessWidget {
+  final List<Room> rooms = [
+    Room(1, 1, 101),
+    Room(1, 1, 102),
+    Room(1, 2, 201),
+    Room(2, 1, 101),
+    Room(2, 2, 201),
+    Room(2, 2, 202),
+  ];
+
+  final List<int> uniqueBuildings = [];
+  final List<int> uniqueFloors = [];
+
+  ExpandableListApp() {
+    _getUniqueBuildingsAndFloors();
+  }
+
+  void _getUniqueBuildingsAndFloors() {
+    for (Room room in rooms) {
+      if (!uniqueBuildings.contains(room.building)) {
+        uniqueBuildings.add(room.building);
+      }
+      if (!uniqueFloors.contains(room.floor)) {
+        uniqueFloors.add(room.floor);
+      }
+    }
+  }
 
   @override
-  Widget build(BuildContext context){
-      return Scaffold(
-        body: SafeArea(
-          child: ListView.builder(
-            itemCount: dataHere.length,
-            itemBuilder:(context, index){
-              return Text('Test');
-            },
-          ),
-          ),
-          );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expandable List'),
+      ),
+      body: ListView(
+        children: [
+          for (int building in uniqueBuildings)
+            ExpansionTile(
+              title: Text('Building: $building'),
+              children: [
+                for (int floor in uniqueFloors)
+                  if (rooms
+                      .where((room) =>
+                  room.building == building && room.floor == floor)
+                      .isNotEmpty)
+                    ExpansionTile(
+                      title: Text('Floor: $floor'),
+                      children: [
+                        Column(
+                          children: _buildRoomTiles(building, floor),
+                        ),
+                      ],
+                    ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
 
+  List<Widget> _buildRoomTiles(int building, int floor) {
+    List<Widget> roomTiles = [];
+
+    for (Room room in rooms) {
+      if (room.building == building && room.floor == floor) {
+        roomTiles.add(
+          ListTile(
+            title: Text('Room: ${room.roomNumber}'),
+          ),
+        );
+      }
+    }
+
+    return roomTiles;
   }
 }
+
+
