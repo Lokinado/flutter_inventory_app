4839ffa9ba62ab74a2a245a478dcde56a8167023
Piotr Bauer <65217796+Bauero@users.noreply.github.com>
2023-05-22 20:36:12 +0200

    Posprzątanie po wprowadzaniu TabBar (#14)
    * login screen z poprawieniem niezgodnosci w plikach
    
    * Testowy Commit (Maciek + Krzysiek)
    
    * Podstawowy test dla ekranu logowania
    
    * naprawiłem bład zwiazany z render overflowed
    
    * Dodałem logowanie i strone główną
    
    * preparation to work on aparat
    
    * preparation to work with camera
    
    W android/app/build.gradle - linia 50 - parametr minSdkVersion | Wstawiłem jakiś kod do scanner page by sprawdzić czy się skompiluje
    
    * first cmaera version
    
    * Alfa working camera
    
    * responsivness and sizes
    
    Wprowadziłem drugą wersję nagłówka - smalltopbody - i poprawiłem skalowanie. dodałem równieź możliwość korzystania z topbodysection jako wzoru - możiwe podanie napisu do wyświetlania jako parametru; dobrałem też inne ikony na dole, z uwzględnieniem tego jakie 3 sekcje powinne znajdować się w naszej aplikacji
    
    * further scaling improvement and cleanum
    
    poprawiłem skalowanie  interfejsu - wielkości przekazywane są z góry przez stronę "scanner_home_page" - umożliwi to odgórne skalowanie elementów w zależności od wielkości przestrzeni roboczej telefonu w przyszłym commicie. Ponadto ulepszyłem nazewnictwom, i przygotowałem oddzielny folder na podstrony do skanera
    
    * final responsive state
    
    W miarę możliwości ustawiłem menu jako responsywne, a teraz przechodzę to ustaiweń treści
    
    * Fully working page change
    
    Możliwa jest w tej chwili zmiana ekranu w z uwzględnieniem zmiany contentu który się na nich znajduje w taki sposób że kliknięcie na odpowiedni przycisk zmienia zawartość i podświetlenie
    
    * Page startup cleanup and rename
    
    Posprzątanie wywoływania stron po wcześniejszych testach a także wprowadzenie adekwatnego nazewnictwa stron
    
    * page division in bottom bar
    
    Podzieleni kodu w ten sposób że każda ze stron którą możemy otworzyć na pana zupełnym znajduje się w oddzielnym pliku - przygotowanie do późniejszego oddzielnego rozwoju innych stron
    
    * Interface improvement
    
    Ujednolicenie interfejsu między trzema różnymi stronami tak aby był on ładny dla użytkownika
    
    * small cleanup
    
    * Separate site folder, body, and cleanup
    
    Zrobiłem porządek wśród utworzonych zmian tak by dało się modyfikować oddzielnie każdą ze stron nie naruszając innych. Wszystkie elementy zostało podzielone według mojego uznania, zielony z rzeczy zgodnie z przeznaczeniem w odpowiednich katalogach. Dodałem także katalog 'prefab' umieściłem elementy które już nie są używane ale mogą posłużyć w przyszłości
    
    * basic marge with Adam
    
    ---------
    
    Co-authored-by: ArekA <arekaffek4@gmail.com>
    Co-authored-by: maciej <macieks53@gmail.com>
    Co-authored-by: Krzysztof Borowski <30807244+Lokinado@users.noreply.github.com>

--- lib/pages/documents/filehomebodypage.dart
+++ lib/pages/documents/filehomebodypage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:inventory_app/components/topbodysection.dart';
+import 'package:inventory_app/pages/documents/details_arguments.dart';
+import 'package:inventory_app/pages/documents/kody.dart';
+import 'package:inventory_app/pages/documents/raporty.dart';
 import 'package:inventory_app/pages/scanner/scanner_page.dart';
 import 'package:inventory_app/components/topbodysection.dart';
 import 'package:inventory_app/components/rounded_rectangle.dart';
@@ -11,38 +15,138 @@ class CenterFileHomePage extends StatelessWidget {
 
   final Size size;
   final Location location;
-  double roundness = 75;
 
 
   @override
-  Widget build(BuildContext context) {
-    var elemWidth = size.width*0.9;
-    return Container(
-      height: size.height*0.7,
-      color: const Color.fromRGBO(0, 50, 39, 1),
-      child: Container(
-        width: size.width,
-        height: size.height,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(
-                location == Location.right? roundness : 0),
-            topLeft: Radius.circular(
-                location == Location.left? roundness : 0),
-          ),
-        ),
-        child: Stack(
-          children: <Widget>[
-            //roundedRectangleTextBox(text: '${34}     |     Budynek', top: 110, right: 5, width: elemWidth),
-            //roundedRectangleTextBox(text: '${3}     |     Piętro', top: 190, right: 5,  width: elemWidth),
-            //roundedRectangleTextBox(text: '${32}      |     Pomieszczenie', top: 270, right: 5,  width: elemWidth),
-            //roundedRectangleButton(destination: CameraPage(), top: 410, right: 5, width: elemWidth, napis: "Coś nowego",),
-            
-          ],
-        ),
-      )
-    );
+  State<CenterFileHomePage> createState() => _CenterFileHomePageState();
+}
 
+class _CenterFileHomePageState extends State<CenterFileHomePage> {
+
+  String _selectedList = '';
+
+  void _showList(String listName) {
+    setState(() {
+      _selectedList = listName;
+    });
+  }
+
+  double roundness = 75;
+
+  @override
+  Widget build(BuildContext context) {
+    var elemWidth = widget.size.width*0.9;
+    return Container(
+      color: const Color.fromRGBO(0, 50, 39, 1),
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () => _showList('Kody'),
+                style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(100, 40)),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return Color.fromRGBO(28, 153, 55, 1);
+                        }
+                        return Color.fromRGBO(26, 98, 49, 1);
+                      },
+                    ),
+                    foregroundColor: MaterialStateProperty.all(Colors.black)
+                ),
+                child: const Text('Kody'),
+
+              ),
+              ElevatedButton(
+                onPressed: () => _showList('Raporty'),
+                style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(100, 40)),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return Color.fromRGBO(28, 153, 55, 1);
+                        }
+                        return Color.fromRGBO(26, 98, 49, 1);
+                      },
+                    ),
+                    foregroundColor: MaterialStateProperty.all(Colors.black)
+                ),
+                child: const Text('Raporty'),
+              ),
+            ],
+          ),
+
+
+
+          _selectedList == ''
+              ? Container()
+              : Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 6),
+              separatorBuilder: (context, index) => const SizedBox(height: 16),
+              itemCount:
+              _selectedList == 'Kody' ? Kody.ilosc : Raporty.ilosc,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Container(
+                    height: 60,
+                    width: 50,
+                    child: ListTile(
+                      title: Text(
+                        _selectedList == 'Kody'
+                            ? 'Kod ${index + 1}'
+                            : 'Raport ${index + 1}',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          '/details',
+                          arguments: DetailsArguments(
+                            title: _selectedList == 'Kody'
+                                ? 'Kod ${index + 1}'
+                                : 'Raport ${index + 1}',
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/pages/documents/kody.dart
@@ -0,0 +1,6 @@
+class Kody {
+  static int ilosc= 20;
+  static String getTitle(int index) {
+    return 'Kod ${index + 1}';
+  }
+}
