6ce827090de567a76c1cf1d3163ea8734a929f40
dawid <dawid.steciuk@gmail.com>
2023-06-07 17:28:17 +0200

    Added left section to the main screen, Currently without data due to lack of database

--- lib/pages/adding/add_page.dart
+++ lib/pages/adding/add_page.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:inventory_app/components/topbodysection.dart';
 import 'package:inventory_app/pages/adding/addhomebody.dart';
+import 'package:inventory_app/pages/list_page.dart';
 
 class AddPage extends StatefulWidget {
   @override
   _AddPageState createState() => _AddPageState();
 }
 
+/*
 class _AddPageState extends State<AddPage> {
   @override
   Widget build(BuildContext context) {
     final Size rozmiar = MediaQuery .of(context) .size;
     return Scaffold(body: ScannerHomeBody(title: "Dodawanie", size: rozmiar, location: Location.left,));
+  }
+}
+*/
+
+class _AddPageState extends State<AddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListPage();//zmien pare rzeczy, góra strony etc
   }
 }
